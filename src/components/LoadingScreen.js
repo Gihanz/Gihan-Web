6320dad8c5826fda226ff73829d8d38fdd83b33e
GIHAN SHAMIKE <gihanzshamike@gmail.com>
2026-01-01 18:06:25 -0500

    Loading logo fixed

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -6,17 +6,17 @@ function LoadingScreen() {
       <div className="relative flex items-center justify-center">
         {/* Profile Image */}
         <img
-          src="gs_loading.gif" // replace with your profile image path
+          src="gs_loading.gif"
           alt="Profile"
-          className="h-24 w-24 rounded-full object-cover z-10"
+          className="h-24 w-24 object-contain z-10"
         />
 
         {/* Spinning Ring */}
-        <div className="absolute animate-spin rounded-full h-32 w-32 border-4 border-gray-500 border-t-transparent"></div>
+        <div className="absolute animate-spin rounded-full h-44 w-44 border-4 border-gray-500 border-t-transparent"></div>
       </div>
 
       {/* Loading Text */}
-      <p className="mt-12 text-lg font-medium text-gray-700 dark:text-gray-200">
+      <p className="mt-20 text-xl font-medium text-gray-700 dark:text-gray-200">
         Loading...
       </p>
     </div>
